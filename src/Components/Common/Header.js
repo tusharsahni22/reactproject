2638090496e6e5dffa582226e131e341cbbef803
swati28-sj <swatijha267@gmail.com>
2023-04-17 23:33:53 +0530

    Sections Added

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,23 +1,40 @@
 import React from 'react' 
 import styled from 'styled-components'
+import { GoSearch } from 'react-icons/go';
+
 
 
 const Option= styled.div`
    color: white;
-   margin: 10px;
+   margin: 15px ;
+   text-align: center;
+   font-family: Arial, Helvetica, sans-serif;
+   font-size: small;
+   
     `;
 const Container = styled.div`
 display: flex;
-justify-content: space-between;
-background-color: darkblue;
-
+background-color: #082340;
+justify-content: space-evenly;
 `;
 const Logo = styled.img`
-height: 50px;
-width: 100px;
+height: 30px;
+width: 130px;
+margin: 15px 0px 10px 0 ;
 `;
 const Text= styled.div`
 display: flex;
+margin: 10px 0 0 100px;
+justify-content: space-between;
+
+
+`;
+const SearchIcon = styled.div`
+color: white;
+border-radius: 5px;
+font-size: 20px;
+margin: 10px 10px 10px 60px;
+
 `;
 
 function Header() {
@@ -25,15 +42,17 @@ function Header() {
     <div>
     <Container>
       <Logo src= "https://www.coforge.com/hs-fs/hubfs/dark_coforge_logo.png?width=627&name=dark_coforge_logo.png"/>
-      <Text>
-
-        
-      <Option>Home </Option>
-      <Option> Product </Option>
-      <Option> company Profile</Option>
-      <Option> Product Gallery </Option>
-      <Option> Contact</Option>
-      <Option> About </Option>
+      
+      <Text>  
+      <Option> About Us </Option>
+      <Option> Indutries </Option>
+      <Option> Services </Option>
+      <Option> Resources </Option>
+      <Option> Investors </Option>
+      <Option> Newsroom </Option>
+      <Option> Carrers </Option>
+      <Option> Contact Us </Option>
+      <SearchIcon><GoSearch/></SearchIcon>
       </Text>
 
     </Container>
