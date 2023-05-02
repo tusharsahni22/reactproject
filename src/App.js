690c476a19c2d109b0210bd4d2c800f8d9831573
Tushar Sahni <tusharsahni22@gmai.com>
2023-05-03 01:51:05 +0545

    header complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Login from './Components/auth/login';
 import Home from './Components/Home';
 import SignUp from './Components/auth/SignUp';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tools from './Components/Tools';
 
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>  
+      <Route path="/" element={<Home/>}/> 
+      <Route path="/tools" element={<Tools/>}/>  
       <Route path="/login" element={<Login/>}/>  
       <Route path="/signup" element={<SignUp/>}/>                 
     </Routes>
---
+++ src/Components/Tools.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import Header from './Common/Header';
+
+const Wrapper = styled.div`
+
+`;
+
+function Tools() {
+  return (
+    <Wrapper>
+        <Header></Header>
+    </Wrapper>
+  )
+}
+
+export default Tools
