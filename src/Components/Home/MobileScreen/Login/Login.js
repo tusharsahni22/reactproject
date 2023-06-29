8523dc598fc3d3ba119b9a05b3f882b464a61381
Swati Jha <swati.jha@gmail.com>
2023-06-29 17:42:23 +0530

    commit

--- src/Components/Home/MobileScreen/Login/Login.js
+++ src/Components/Home/MobileScreen/Login/Login.js
@@ -30,7 +30,7 @@ function Login() {
   return (   
       <Heads>
          <AiOutlineArrowLeft/>
-        <div> Login </div>
+        <div> LoginDiv </div>
         <Div> 
          <BsSearch/>  
          <BiDotsVerticalRounded/>        
