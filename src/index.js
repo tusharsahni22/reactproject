faa34bc5b5866e9a3fbc72f6072b4bc3bd4df8be
Tushar Sahni <tusharsahni22@.com>
2023-09-11 15:47:34 +0530

    background fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./Router";
+import styled from "styled-components";
+
+const MobBackgroundColour = styled.div`
+@media(max-width:767px){
+      background-color:  rgb(34,34,34);
+      color:white;
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+root.render(<MobBackgroundColour><Router/></MobBackgroundColour>);
